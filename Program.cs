34ef331d6926f2ba3a4a2eaf70ebaf38f689215f
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-06-04 01:43:55 -0300

    resolvido parte 2

--- Program.cs
+++ Program.cs
@@ -2,36 +2,42 @@
 
 class Program
 {
-    static double CalcularRazao(double etanol, double gasolina)
+    static double calcRazao(double etanol, double gasolina)
     {
         return etanol / gasolina;
+        //Efetua o calculo da razão entre etanol e gasolina.
     }
 
-    static bool VerificarRecomendacao(double razao)
+    static bool verifyRecomendacao(double razao)
     {
         double percentualLimite = 0.73;
         return razao <= percentualLimite;
+        //Define qual a recomendação a dar, ou seja, abastecer com gasolina ou álcool.
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Informe o preço do etanol:");
+        
+        Console.WriteLine("Digite o preço do etanol (em R$). ");
         double precoEtanol = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Informe o preço da gasolina:");
+        Console.WriteLine("Digite o preço da gasolina (em R$). ");
         double precoGasolina = Convert.ToDouble(Console.ReadLine());
 
-        double razao = CalcularRazao(precoEtanol, precoGasolina);
+        double razao = calcRazao(precoEtanol, precoGasolina);
         double razaoPorcentagem = razao * 100;
+        
         Console.WriteLine("");
         Console.WriteLine("Razão entre os preços: " + razaoPorcentagem.ToString("F0") + "%");
 
-        bool recomendacao = VerificarRecomendacao(razao);
+        bool recomendacao = verifyRecomendacao(razao);
+        
         if (recomendacao)
         {
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Recomenda-se abastecer com gasolina.");
         }
+        
         else
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
